f14bf86b76c3b3562b1a259fe4c3ff5ceffbc7bc
Павел Баев <pavel.baev44@gmail.com>
2025-06-15 01:43:49 +0300

    feat:update message permission error to participants

--- participants/permissions.py
+++ participants/permissions.py
@@ -1,41 +1,42 @@
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import BasePermission
 
 from participants.enums import ParticipantRole
+from participants.models import Participant
 
-from participants.utils import check_contest_role_permission
+
+class BaseContestRolePermission(BasePermission):
+    role = None
+    message = "Недостаточно прав для выполнения действия."
+
+    def has_permission(self, request, view):
+        contest_id = getattr(request, 'contest_id', None)
+        user = request.user
+
+        if not contest_id:
+            raise PermissionDenied("Контекст конкурса не указан.")
+
+        if not Participant.objects.filter(
+            contest_id=contest_id,
+            user_id=user.id,
+            role=self.role.value,
+        ).exists():
+            raise PermissionDenied(f"Для этого действия требуется роль: {self.role.label}")
+
+        return True
 
 
-class IsContestOwnerPermission(BasePermission):
-    def has_permission(self, request, view) -> bool:
-        return check_contest_role_permission(
-            contest_id=request.contest_id,
-            user_id=request.user.id,
-            role=ParticipantRole.owner,
-        )
+class IsContestOwnerPermission(BaseContestRolePermission):
+    role = ParticipantRole.owner
 
 
 class IsContestJuryPermission(BasePermission):
-    def has_permission(self, request, view) -> bool:
-        return check_contest_role_permission(
-            contest_id=request.contest_id,
-            user_id=request.user.id,
-            role=ParticipantRole.jury,
-        )
+    role = ParticipantRole.jury
 
 
 class IsContestMemberPermission(BasePermission):
-    def has_permission(self, request, view) -> bool:
-        return check_contest_role_permission(
-            contest_id=request.contest_id,
-            user_id=request.user.id,
-            role=ParticipantRole.member,
-        )
+    role = ParticipantRole.member
 
 
 class IsOrgCommitteePermission(BasePermission):
-    def has_permission(self, request, view) -> bool:
-        return check_contest_role_permission(
-            contest_id=request.contest_id,
-            user_id=request.user.id,
-            role=ParticipantRole.org_committee,
-        )
+    role = ParticipantRole.org_committee
